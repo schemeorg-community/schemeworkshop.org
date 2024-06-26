158a32de273181292e16b974d04aec1c6ed14856
Lassi Kortela <lassi@lassi.io>
2024-06-26 20:22:32 +0300

    Comment upload scripts

--- scripts/upload-staging-www.sh
+++ scripts/upload-staging-www.sh
@@ -3,4 +3,5 @@ set -eu
 cd "$(dirname "$0")"
 cd ..
 set -x
+# Do not use --delete because papers are uploaded from another repository.
 rsync -vcr --exclude="*~" www/ tuonela.scheme.org:/staging/workshop/www/
